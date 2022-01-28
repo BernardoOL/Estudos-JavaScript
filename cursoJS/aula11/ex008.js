943c065d397d9a8716db905f59c554365528a48d
Bernardo Lopes <bernardomak42@gmail.com>
2022-01-28 11:54:24 -0300

    novos exercícios

---
+++ cursoJS/aula11/ex008.js
@@ -0,0 +1,11 @@
+var velocidade = 60
+
+if(velocidade >80){
+    console.log(`Velocidade = ${velocidade} Km/h`);
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+    console.log(`Dirija sempre usando o cinto de segurança!`)
+}else{
+    console.log(`Velocidade = ${velocidade}`);
+    console.log(`Você está dentro da velocidade da via`)
+    console.log(`Dirija sempre usando o cinto de segurança!`)
+}
